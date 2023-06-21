5555090b5704601abdd1f22e9c56b300b2b841e1
aamcodes <aaronmendezcodes@gmail.com>
2023-06-21 14:20:28 -0400

    installed express session package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
+const session = require('express-session');
 
 const apiRouter = require('./routes');
 
