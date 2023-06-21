44d349d87ce549ac389d507c3a2324f96f327e9c
Nrochester <123398837+Nrochester@users.noreply.github.com>
2023-06-21 15:06:59 -0400

    made updates to routes

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { Users } = require("../schema/models/users")
 
-router.get('/', (req, res) => {
+// GET - Retrieve all Users
+router.get("/", async (req, res) => {
 	try {
-		// do here
+	  let user = await Users.findAll();
+	  res.status(200).json({ user });
 	} catch (err) {
-		res.status(500).json({ error: 'Server Internal Error' });
+	  res.status(500).json({ message: "Server Internal Error", error: err });
 	}
-});
+  });
 
 module.exports = router;
