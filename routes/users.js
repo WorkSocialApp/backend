a9b3e54c58f4357fe9f32b11194bf5bca25d3729
ciaraerica23 <ciaraestafford@gmail.com>
2023-06-26 14:48:04 -0400

    fixed routes

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,12 @@ const { verifyAuth } = require('../middleware/verifyAuth');
 // GET - Retrieve all Users
 router.get("/", verifyAuth, async (req, res) => {
 	try {
-	  let user = await Users.findAll().then((allUsers) => {
-		  res.status(200).json({ user });
-
-	  })
-	  .catch((error) => {
-		  res.status(500).json({ message: "Server Internal Error", error: err });
-
-	  })
-	} catch (err){}
-  });
-
+	  let users = await Users.findAll()  
+		res.send(users)
+    res.status(200).json({ message:"success"});
+  }
+  catch(error) {
+	res.status(500).json({ message: "Server Internal Error", error: err });
+  }
+});
 module.exports = router;
